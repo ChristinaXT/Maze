0398e01f5509a6b7538351f1e705f829efe8c6b3
Christina Williams <christinaxtwilliams@gmail.com>
2021-03-11 16:16:59 -0500

    refactor

--- index.js
+++ index.js
@@ -26,24 +26,3 @@ const walls = [
   Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
 ];
 World.add(world, walls);
-
-//Random Shapes
-
-for (let i = 0; i < 50; i++) {
-  if (Math.random() > 0.5) {
-  World.add(
-    world,
-    Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
-  );
- } else {
-   World.add(
-     world,
-     Bodies.circle(Math.random() * width, Math.random() * height, 35, {
-       render: {
-         fillStyle: 'cyan'
-       }
-     })
-   );
-
- }
-}
