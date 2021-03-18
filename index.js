9e44481a41451c5f47898e7ef2e8bafef4dbf897
Christina Williams <christinaxtwilliams@gmail.com>
2021-03-18 14:11:54 -0400

    shuffle

--- index.js
+++ index.js
@@ -88,14 +88,3 @@ const recurseMaze = (row, column) => {
 };
 
 recurseMaze(startRow, startColumn)
-
-// const grid = [];
-//
-// for (let i = 0; i < 3; i++) {
-//   grid.push([]);
-//   for (let j = 0; j < 3; j++) {
-//     grid[i].push(false);
-//   }
-// }
-//
-// console.log(grid);
