a0c46b87270b3026f74ed23b954218bd27f4fec3
Christina Williams <christinaxtwilliams@gmail.com>
2021-03-19 14:08:09 -0400

    shuffle

--- index.js
+++ index.js
@@ -67,6 +67,36 @@ const recurseMaze = (row, column) => {
     return;
   }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   // Mark this cell as being visited
   grid[row][column] = true;
   // Assemble randomly-ordered list of neighbors
